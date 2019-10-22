be8a7d775ec43e0cadf98b6014bcb4ee42cc7508
Pushpinder Pal Singh <thisisme@pushpinderpalsingh.com>
2019-10-23 00:00:22 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Pushpinder Pal Singh on 23/10/19.
+//  Copyright © 2019 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
